4a3b66cb95bd432316dce1c36b61ddf9b851ea5b
mackwilson@github.com <ms2wilso@edu.uwaterloo.ca>
2020-05-19 11:09:15 -0400

    Updates

---
+++ precache-manifest.cdc3c954e7b63b9e430bd956475139b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130d56dbc6adfa5e245ef55b5004b2d8",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "7090aaae63db7c1134aa",
+    "url": "/about/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af7fce1acb9f74fa399d",
+    "url": "/about/static/js/2.38dd8650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about/static/js/2.38dd8650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7090aaae63db7c1134aa",
+    "url": "/about/static/js/main.6ddd7bb3.chunk.js"
+  },
+  {
+    "revision": "77854eb316008fc648f3",
+    "url": "/about/static/js/runtime-main.c7fcc918.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about/static/media/logo.5d5d9eef.svg"
+  }
+]);
