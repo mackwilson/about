f4853b4eb2615ba0c2c6c43957aa323218b2f525
mackwilson <ms2wilso@uwaterloo.ca>
2020-06-01 11:46:28 -0400

    Updates

---
+++ precache-manifest.917467096ebba6975a2c0f4ecf20d765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382e71f85967b3c041fc5ba43ea380e7",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "7d3903bc07783374a4d4",
+    "url": "/about/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "26f62057adcaf39a3fea",
+    "url": "/about/static/css/main.19c54f5e.chunk.css"
+  },
+  {
+    "revision": "7d3903bc07783374a4d4",
+    "url": "/about/static/js/2.7bdc6f9a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/about/static/js/2.7bdc6f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f62057adcaf39a3fea",
+    "url": "/about/static/js/main.40f5b639.chunk.js"
+  },
+  {
+    "revision": "77854eb316008fc648f3",
+    "url": "/about/static/js/runtime-main.c7fcc918.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about/precache-manifest.cdc3c954e7b63b9e430bd956475139b8.js"
+  "/about/precache-manifest.917467096ebba6975a2c0f4ecf20d765.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/about/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
