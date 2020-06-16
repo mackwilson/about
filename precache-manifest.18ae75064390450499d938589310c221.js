fb96233a591519b1894097d37e996aa77f2f0d00
mackwilson <ms2wilso@uwaterloo.ca>
2020-06-15 22:52:44 -0400

    Updates

---
+++ precache-manifest.18ae75064390450499d938589310c221.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d47ed27eebe3ebebd79e8265dc1c218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab3fa175b0716fc6230b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "969b98ea9dbd9bc0b07f",
+    "url": "/static/css/main.2ac636d1.chunk.css"
+  },
+  {
+    "revision": "ab3fa175b0716fc6230b",
+    "url": "/static/js/2.d0a27f4d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d0a27f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969b98ea9dbd9bc0b07f",
+    "url": "/static/js/main.72010e62.chunk.js"
+  },
+  {
+    "revision": "590e9f4cc22c98a9cac0",
+    "url": "/static/js/runtime-main.f852fa46.js"
+  },
+  {
+    "revision": "e9d965e26fa09fe662c2436c57ac934a",
+    "url": "/static/media/Resume.e9d965e2.pdf"
+  }
+]);
