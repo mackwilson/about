7e7dff4e05b460805351ecc9b3a3911a3d2bf0b7
mackwilson <ms2wilso@uwaterloo.ca>
2020-06-21 23:38:06 -0400

    Navbar functionality and collapse

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './pages/Header';
-import Navbar from './comps/Navbar';
+import Menu from './comps/Menu';
 import About from './pages/About';
 import Experience from './pages/Experience';
 import Projects from './pages/Projects';
@@ -12,7 +12,7 @@ import './css/App.css';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Menu />
       <div className="appBody">
         <Header />
         <About />
---
+++ src/comps/Menu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+// import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import logo from '../assets/logo.png';
+
+function Menu() {
+  // return (
+  //   <div className="Navbar">
+  //     <Button variant="outline-secondary" className="navBtn" href="#About" >
+  //       About
+  //     </Button>
+  //     <Button variant="outline-secondary" className="navBtn" href="#Experience" >
+  //       Experience
+  //     </Button>
+  //     <Button variant="outline-secondary" className="navBtn" href="#Projects" >
+  //       Projects
+  //     </Button>
+  //     <Button variant="outline-secondary" className="navBtn" href="#Interests" >
+  //       Interests
+  //     </Button>
+  //     <Button variant="outline-secondary" className="navBtn" href="#Contact" >
+  //       Contact
+  //     </Button>
+  //   </div>
+  // );
+
+  return (
+    <Navbar className="Menu" expand="md">
+    <Navbar.Brand href="#Header">
+      <img
+        alt=""
+        src={logo}
+        width="40"
+        height="40"
+        className="d-inline-block align-top menu-logo"
+      />{' '}
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav">
+      <i class="material-icons">menu</i>
+    </Navbar.Toggle>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav>
+        <Nav.Link className="navBtn" href="#About">About</Nav.Link>
+        <Nav.Link className="navBtn" href="#Experience">Experience</Nav.Link>
+        <Nav.Link className="navBtn" href="#Projects">Projects</Nav.Link>
+        <Nav.Link className="navBtn" href="#Interests">Interests</Nav.Link>
+        <Nav.Link className="navBtn" href="#Contact">Contact</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  );
+}
+
+
+export default Menu;
