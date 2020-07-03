aa4e0f690ecde7b4f2f2b9ed8c6025721b4fac6b
mackwilson <ms2wilso@uwaterloo.ca>
2020-07-03 16:45:48 -0400

    Updates

---
+++ precache-manifest.3356bff07c7da966e396985fa990ccb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d3f9ea2483afc8a15778a4d6fdf41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f3a5856f93cca1d5c02",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dcfc0457ec08f2879971",
+    "url": "/static/css/main.b93552d7.chunk.css"
+  },
+  {
+    "revision": "1f3a5856f93cca1d5c02",
+    "url": "/static/js/2.9e09fd8d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9e09fd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfc0457ec08f2879971",
+    "url": "/static/js/main.72489887.chunk.js"
+  },
+  {
+    "revision": "590e9f4cc22c98a9cac0",
+    "url": "/static/js/runtime-main.f852fa46.js"
+  },
+  {
+    "revision": "e9d965e26fa09fe662c2436c57ac934a",
+    "url": "/static/media/Resume.e9d965e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about/precache-manifest.5fd55e9af69b684c0b4b70d572e45321.js"
+  "/precache-manifest.3356bff07c7da966e396985fa990ccb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/about/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
